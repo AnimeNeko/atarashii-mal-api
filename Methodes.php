80e5fa03a414323d3ed5ccb469fde282f25c80ee
ratan12 <ratandhawtal@hotmail.com>
2013-11-02 22:41:02 +0100

    Added UserAgent

--- Methodes.php
+++ Methodes.php
@@ -10,6 +10,7 @@
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curl, CURLOPT_HEADER, false);
+	curl_setopt($curl, CURLOPT_USERAGENT, "Atarashii");
     $data = curl_exec($curl);
     curl_close($curl);
     return ($data);
