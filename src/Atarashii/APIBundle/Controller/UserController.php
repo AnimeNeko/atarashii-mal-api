2b162f4a14e1472ab131d8374c87b316b129bcc3
Michael Johnson <youngmug@animeneko.net>
2013-11-10 08:39:05 -0800

    Remove Debug Info from UserController
    Accidentally left in some working debug stuff from when testing the
    user profile fetcher. Removing this so the code will work correctly.

--- src/Atarashii/APIBundle/Controller/UserController.php
+++ src/Atarashii/APIBundle/Controller/UserController.php
@@ -24,8 +24,6 @@ class UserController extends FOSRestController
 		$downloader = $this->get('atarashii_api.downloader');
 		$profilecontent = $downloader->fetch('/profile/' . $username);
 
-		print $profilecontent; die();
-
  		$userprofile = new User();
   		$userprofile->parse($profilecontent);
 
