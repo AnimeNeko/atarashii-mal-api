ebc350715d277745390e893943dffefeef66fd77
ratan12 <ratandhawtal@hotmail.com>
2013-11-12 15:50:46 +0100

    Added Popular
    Add route for popular list and set up controller and model.

---
+++ src/Atarashii/APIBundle/Controller/PopularController.php
@@ -0,0 +1,45 @@
+<?php
+namespace Atarashii\APIBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Atarashii\APIBundle\Model\Popular;
+
+use \SimpleXMLElement;
+
+class PopularController extends FOSRestController
+{
+
+    /**
+     * Profile get action
+     * @var string $page page number(start at 1) 
+     * @return array
+     *
+     * @Rest\View()
+     */
+	public function getAnimeAction($page)
+	{
+		#http://myanimelist.net/topanime.php?type=bypopularity&limit=#{0}
+
+		$downloader = $this->get('atarashii_api.downloader');
+		$animecontent = $downloader->fetch('/topanime.php?type=bypopularity&limit='.(($page*30)-30));
+
+ 		$popularanime = new Popular();
+  		$popularanime = $popularanime->parse($animecontent,'anime');
+ 		return $popularanime;
+	}
+	
+	public function getMangaAction($page)
+	{
+		#http://myanimelist.net/topmanga.php?type=bypopularity&limit=#{0}
+		
+		$downloader = $this->get('atarashii_api.downloader');
+		$mangacontent = $downloader->fetch('/topmanga.php?type=bypopularity&limit='.(($page*30)-30));
+
+ 		$popularmanga = new Popular();
+  		$popularmanga->parse($mangacontent,'manga');
+
+ 		return $popularmanga;
+	}
+
+}
---
+++ src/Atarashii/APIBundle/Model/Popular.php
@@ -0,0 +1,90 @@
+<?php
+namespace Atarashii\APIBundle\Model;
+
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\CssSelector\CssSelector;
+
+class Popular {
+
+	public function parse($contents,$type) {
+		$crawler = new Crawler();
+		$crawler->addHTMLContent($contents, 'UTF-8');
+		$maincontent = $crawler->filter('#horiznav_nav')->nextAll();
+		
+		//decides which type it is.
+		if (strpos($type,'anime') !== false){
+			return ($this->parseAnime($maincontent));
+		}else{
+			$this->anime_stats = $this->parseStats($maincontent);
+		}
+	}
+
+	private function parseAnime($content) {
+
+	$elements = $content->filterXPath('//table[1]')->filter('tr');
+	$array = array();
+	
+		foreach ($elements as $content) {
+
+			$crawler = new Crawler($content);
+			$details = new AnimeDetails();
+			$id = str_replace('#area','',$crawler->filter('a')->attr('id'));
+			$title = trim($crawler->filter('strong')->text());
+			//I removed the 't' because it will return a little image
+			$image_url = str_replace('t.j','.j',$crawler->filter('img')->attr('src'));
+			$members_count = str_replace(' members','',trim($crawler->filterXPath('//td[3]')->filter('span')->text()));
+			
+			//Info contains: TV, 37 eps, scored 8.82
+			$info = trim($crawler->filterXPath('//td[3]//div[2]')->text());
+			//Get the type
+			$type = 'Error';
+			if (strpos($info,'TV') !== false){
+				$type = 'TV';
+			}elseif (strpos($info,'OVA') !== false){
+				$type = 'OVA';
+			}elseif (strpos($info,'Movie') !== false){
+				$type = 'Movie';
+			}elseif (strpos($info,'Special') !== false){
+				$type = 'Special';
+			}elseif (strpos($info,'ONA') !== false){
+				$type = 'ONA';
+			}elseif (strpos($info,'Music') !== false){
+				$type = 'Music';
+			}
+			//Get the episodes number & score
+			$episodes = $this->parsestring($info,$type.', ',' eps,');
+			$score = str_replace($members_count.' members','',str_replace($type.', '.$episodes.' eps, scored ','',$info));
+			
+			//Setting up the AnimeDetails
+			$key ='id';$details->$key = $id;
+			$key ='title';$details->$key = $title;
+			$key ='image_url';$details->$key = $image_url;
+			$key ='members_count';$details->$key = $members_count;
+			$key ='type';$details->$key = $type;
+			$key ='episodes';$details->$key = $episodes;
+			$key ='score';$details->$key = $score;
+			
+			array_push($array, $details);
+		}
+		return $array;
+	}
+	
+	// $string = the string wich contains the text, $first = before the text, $second = after the text
+	function parsestring($string,$first,$second){
+		$startsAt = strpos($string, $first);
+		$endsAt = strpos($string, $second, $startsAt);
+		$parse = substr($string, $startsAt, $endsAt - $startsAt);
+		$parsed = str_replace($first, '', $parse);
+		return($parsed);
+	}
+}
+
+Class AnimeDetails {
+	public $id;
+	public $title;
+	public $image_url;
+	public $members_count;
+	public $type;
+	public $episodes;
+	public $score;
+}
