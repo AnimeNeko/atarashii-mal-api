0135968b5a63739bd6e47eed0936754c0d17c0af
ratan12 <ratandhawtal@hotmail.com>
2013-11-23 16:34:13 +0100

    Merged (& cleanup) Just added and Upcoming.
    The parsing methodes (and the lay-out) were the same just like top and popular.
    I merged them and done a cleanup (using micheals methods).(fixes #14)

--- src/Atarashii/APIBundle/Controller/UpcomingController.php
+++ src/Atarashii/APIBundle/Controller/UpcomingController.php
@@ -17,42 +17,79 @@ class UpcomingController extends FOSRestController
      *
      * @Rest\View()
      */
-	public function getAnimeAction(Request $request)
+	public function getAnimeUpcomingAction(Request $request)
 	{
 		#http://myanimelist.net/anime.php?sd=#{day}&sm=#{month}&sy=#{year}&em=0&ed=0&ey=0&o=2&w=&c[]=a&c[]=d&c[]=a&c[]=b&c[]=c&c[]=d&c[]=e&c[]=f&c[]=g&cv=1&show=#{page}
 
 		$page = (int) $request->query->get('page');
 
-		if ($page <= 0)
-		{
+		if ($page <= 0)	{
 			$page = 1;
 		}
 
 		$downloader = $this->get('atarashii_api.downloader');
 		$animecontent = $downloader->fetch('/anime.php?sd='.date("j").'&sm='.date("n").'&sy='.date("y").'&em=0&ed=0&ey=0&o=2&w=&c[]=a&c[]=d&c[]=a&c[]=b&c[]=c&c[]=d&c[]=e&c[]=f&c[]=g&cv=1&show='.(($page*20)-20));
- 		$Upcominganime = new Upcoming();
-  		$Upcominganime = $Upcominganime->parse($animecontent,'anime');
+ 		
+		$Upcominganime = Upcoming::parse($animecontent,'anime');
  		return $Upcominganime;
 	}
 
-	public function getMangaAction(Request $request)
+	public function getMangaUpcomingAction(Request $request)
 	{
 		#http://myanimelist.net/manga.php?sd=#{day}&sm=#{month}&sy=#{year}&em=0&ed=0&ey=0&o=2&w=&c[]=a&c[]=d&c[]=a&c[]=b&c[]=c&c[]=d&c[]=e&c[]=f&c[]=g&cv=1&show=#{page}
 
 		$page = (int) $request->query->get('page');
 
-		if ($page <= 0)
-		{
+		if ($page <= 0)	{
 			$page = 1;
 		}
 
 		$downloader = $this->get('atarashii_api.downloader');
 		$mangacontent = $downloader->fetch('/manga.php?sd='.date("j").'&sm='.date("n").'&sy='.date("y").'&em=0&ed=0&ey=0&o=2&w=&c[]=a&c[]=d&c[]=a&c[]=b&c[]=c&c[]=d&c[]=e&c[]=f&c[]=g&cv=1&show='.(($page*20)-20));
 
- 		$Upcomingmanga = new Upcoming();
-  		$Upcomingmanga = $Upcomingmanga->parse($mangacontent,'manga');
+ 		$Upcomingmanga = Upcoming::parse($mangacontent,'manga');
 
  		return $Upcomingmanga;
 	}
 
+	/**
+     * Justadded get action
+     * @return array
+     *
+     * @Rest\View()
+     */
+	public function getAnimeJustaddedAction(Request $request)
+	{
+		#http://myanimelist.net/anime.php?o=9&c[]=a&c[]=b&c[]=c&c[]=d&c[]=e&c[]=f&c[]=g&cv=2&w=1&show=#{page}
+
+		$page = (int) $request->query->get('page');
+
+		if ($page <= 0){
+			$page = 1;
+		}
+
+		$downloader = $this->get('atarashii_api.downloader');
+		$animecontent = $downloader->fetch('/anime.php?o=9&c[]=a&c[]=b&c[]=c&c[]=d&c[]=e&c[]=f&c[]=g&cv=2&w=1&show='.(($page*20)-20));
+
+  		$Justaddedanime = Upcoming::parse($animecontent,'anime');
+ 		return $Justaddedanime;
+	}
+
+	public function getMangaJustaddedAction(Request $request)
+	{
+		#http://myanimelist.net/anime.php?o=9&c[]=a&c[]=b&c[]=c&c[]=d&c[]=e&c[]=f&c[]=g&cv=2&w=1&show=#{page}
+
+		$page = (int) $request->query->get('page');
+
+		if ($page <= 0){
+			$page = 1;
+		}
+
+		$downloader = $this->get('atarashii_api.downloader');
+		$mangacontent = $downloader->fetch('/manga.php?o=9&c[]=a&c[]=b&c[]=c&c[]=d&c[]=e&c[]=f&c[]=g&cv=2&w=1&show='.(($page*20)-20));
+
+ 		$Justaddedmanga = Upcoming::parse($mangacontent,'manga');
+
+ 		return $Justaddedmanga;
+	}
 }
--- src/Atarashii/APIBundle/Parser/Upcoming.php
+++ src/Atarashii/APIBundle/Parser/Upcoming.php
@@ -8,83 +8,68 @@ use Atarashii\APIBundle\Model\Manga;
 
 class Upcoming {
 
-	public function parse($contents,$type) {
+	public static function parse($contents,$type) {
 		$crawler = new Crawler();
 		$crawler->addHTMLContent($contents, 'UTF-8');
-		$maincontent = $crawler->filter('.borderClass')->first()->nextAll();
-
-		//decides which type it is.
-		if (strpos($type,'anime') !== false){
-			return ($this->parserecord($maincontent,'Anime'));
-		}else{
-			return ($this->parserecord($maincontent,'Manga'));
-		}
-	}
-
-	private function parserecord($content,$typeserie) {
-	$searchbar = true;
-	$check = true;
-	$elements = $content->filter('tr');
-
-	$array = array();
-
-		foreach ($elements as $content) {
-			//tricky method to skip the search bar which also is a <tr></tr>
-			if ($check == true){
-				$check = false;
+		$menubar = true;
+		
+		//Filter into a set of tds from the source HTML table
+		$mediaitems = $crawler->filter('#horiznav_nav')->nextAll()->filterXPath('./div/table/tr');
+		
+		foreach($mediaitems as $item) {
+			//tricky methode to skip the menu bar which is also a <tr></tr>
+			if ($menubar == true){
+				$menubar = false;
 			}else{
-				$crawler = new Crawler($content);
-				if (strpos($typeserie,'Anime') !== false){
-					$details = new Anime();
-
-					//Custom parsing
-					$synopsis = trim($crawler->filterXPath('//td[2]//div[3]')->text());
-					$episodes = trim($crawler->filterXPath('//td[4]')->text());
-					$members_score = trim($crawler->filterXPath('//td[5]')->text());
-					$start_date = trim($crawler->filterXPath('//td[6]')->text());
-					$end_date = trim($crawler->filterXPath('//td[7]')->text());
-					$classification = trim($crawler->filterXPath('//td[9]')->text());
-					$synopsis = trim($crawler->filterXPath('//td[2]//div[3]')->text());
-
-					//Setting up the AnimeDetails
-					$key ='episodes';$details->$key = $episodes;
-					$key ='classification';$details->$key = $classification;
-				}else{
-					$details = new Manga();
-
-					//Custom parsing
-					$type = trim($crawler->filterXPath('//td[3]')->text());
-					$volumes = trim($crawler->filterXPath('//td[4]')->text());
-					$chapters = trim($crawler->filterXPath('//td[5]')->text());
-					$members_score = trim($crawler->filterXPath('//td[6]')->text());
-					$start_date = trim($crawler->filterXPath('//td[7]')->text());
-					$end_date = trim($crawler->filterXPath('//td[8]')->text());
-					$synopsis = trim($crawler->filterXPath('//td[2]//div[2]')->text());
-
-					//Setting up the MangaDetails
-					$key ='volumes';$details->$key = $volumes;
-					$key ='chapters';$details->$key = $chapters;
-				}
-				//universal parsing
-				$id = str_replace('#sarea','',$crawler->filter('a')->attr('id'));
-				$title = $crawler->filter('strong')->text();
-				//I removed the 't' because it will return a little image
-				$image_url = str_replace('t.j','.j',$crawler->filter('img')->attr('src'));
-				$type = trim($crawler->filterXPath('//td[3]')->text());
-
-				//universal details
-				$key ='id';$details->$key = $id;
-				$key ='title';$details->$key = $title;
-				$key ='type';$details->$key = $type;
-				$key ='synopsis';$details->$key = $synopsis;
-				$key ='image_url';$details->$key = $image_url;
-				$key ='members_score';$details->$key = $members_score;
-				$key ='start_date';$details->$key = $start_date;
-				$key ='end_date';$details->$key = $end_date;
-
-				array_push($array, $details);
+				$resultset[] = self::parseRecord($item, $type);
 			}
 		}
-		return $array;
+		return $resultset;
+	}
+
+	private static function parserecord($item,$type) {
+		$crawler = new Crawler($item);
+		$check = true;
+
+		//Get the type record.
+		switch($type) {
+			case 'anime':
+				$media = new Anime();
+				break;
+			case 'manga':
+				$media = new Manga();
+				break;
+		}
+
+		//Pull out all the common parts
+		$media->id = (int) str_replace('#sarea','',$crawler->filter('a')->attr('id'));
+		$media->title = $crawler->filter('strong')->text();
+		//I removed the 't' because it will else return a little image
+		$media->image_url = str_replace('t.j','.j',$crawler->filter('img')->attr('src'));
+		$media->type = trim($crawler->filterXPath('//td[3]')->text());
+		
+		switch($type) {
+			case 'anime':
+				//Custom parsing for anime
+				$media->episodes = (int) trim($crawler->filterXPath('//td[4]')->text());
+				$media->start_date = trim($crawler->filterXPath('//td[6]')->text());
+				$media->end_date = trim($crawler->filterXPath('//td[7]')->text());
+				$media->classification = trim($crawler->filterXPath('//td[9]')->text());
+				$media->members_score = (float) trim($crawler->filterXPath('//td[5]')->text());
+				$media->synopsis = trim($crawler->filterXPath('//td[2]//div[3]')->text());
+				break;
+			case 'manga':
+				//Custom parsing for manga
+				$media->type = trim($crawler->filterXPath('//td[3]')->text());
+				$media->chapters = (int) trim($crawler->filterXPath('//td[5]')->text());
+				$media->volumes = (int) trim($crawler->filterXPath('//td[4]')->text());
+				$media->members_score = (float) trim($crawler->filterXPath('//td[6]')->text());
+				$media->synopsis = trim($crawler->filterXPath('//td[2]//div[2]')->text());
+				//$start_date = trim($crawler->filterXPath('//td[7]')->text());
+				//$end_date = trim($crawler->filterXPath('//td[8]')->text());
+				break;
+			}
+		
+		return $media;
 	}
 }
