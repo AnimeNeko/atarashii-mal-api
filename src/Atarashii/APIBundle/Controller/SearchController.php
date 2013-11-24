0c02c6f27bbf8fb7679a7ceba5d86dfa9086f169
ratan12 <ratandhawtal@hotmail.com>
2013-11-24 16:42:31 +0100

    Cleanup search & removed the parser (using upcoming parser currently).
    I removed the parser becouse it was the exact the same as the upcoming parser.

--- src/Atarashii/APIBundle/Controller/SearchController.php
+++ src/Atarashii/APIBundle/Controller/SearchController.php
@@ -4,7 +4,7 @@ namespace Atarashii\APIBundle\Controller;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Request;
-use Atarashii\APIBundle\Parser\Search;
+use Atarashii\APIBundle\Parser\Upcoming;
 
 use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\CssSelector\CssSelector;
@@ -12,25 +12,22 @@ use Symfony\Component\CssSelector\CssSelector;
 use \SimpleXMLElement;
 use \DateTime;
 
-class SearchController extends FOSRestController
-{
+class SearchController extends FOSRestController {
 
 	/**
      * Search results
      * @var string $page text to search
      * @return array
      */
-	public function getAnimeAction(Request $request)
-	{
+	public function getAnimeAction(Request $request){
 		#http://myanimelist.net/anime.php?c[]=a&c[]=b&c[]=c&c[]=d&c[]=e&c[]=f&c[]=g&q=#{name}
 
 		$page = $request->query->get('q');
-
+		
 		$downloader = $this->get('atarashii_api.downloader');
 		$animecontent = $downloader->fetch('/anime.php?c[]=a&c[]=b&c[]=c&c[]=d&c[]=e&c[]=f&c[]=g&q='.$page);
 
- 		$searchanime = new Search();
-  		$searchanime = $searchanime->parse($animecontent,'anime');
+ 		$searchanime = Upcoming::parse($animecontent,'anime');
 
  		return $searchanime;
 	}
@@ -44,8 +41,7 @@ class SearchController extends FOSRestController
 		$downloader = $this->get('atarashii_api.downloader');
 		$mangacontent = $downloader->fetch('/manga.php?c[]=a&c[]=b&c[]=c&c[]=d&c[]=e&c[]=f&c[]=g&q='.$page);
 
- 		$searchmanga = new Search();
-  		$searchmanga = $searchmanga->parse($mangacontent,'manga');
+ 		$searchmanga = Upcoming::parse($mangacontent,'manga');
 
  		return $searchmanga;
 	}
