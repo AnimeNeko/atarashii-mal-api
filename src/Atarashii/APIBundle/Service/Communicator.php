9054a604a6784204c8305023ef951430739cf58d
Michael Johnson <youngmug@animeneko.net>
2013-12-13 12:22:55 -0800

    Add Cookie Login Support
    Create a method that will post a login to MAL to grab the login cookies.
    This method needs to be called before using fetch or send if the code is
    using a front-end page that requires a site login. It is not needed for
    direct MAL API calls that use basic auth.
    
    Closes #15

--- src/Atarashii/APIBundle/Service/Communicator.php
+++ src/Atarashii/APIBundle/Service/Communicator.php
@@ -2,17 +2,36 @@
 namespace Atarashii\APIBundle\Service;
 
 use Guzzle\Http\Client;
+use Guzzle\Plugin\Cookie\CookiePlugin;
+use Guzzle\Plugin\Cookie\CookieJar\ArrayCookieJar;
 
 class Communicator {
 	private $useragent;
 	private $baseurl;
 	private $client;
+	private $cookies;
 
 	function __construct($baseurl, $ua) {
 		$this->useragent = $ua;
+		$this->cookies = new CookiePlugin(new ArrayCookieJar());
 
 		// create http client instance
 		$this->client = new Client($baseurl);
+		$this->client->addSubscriber($this->cookies);
+	}
+
+	public function cookieLogin($username, $password) {
+		// create a request
+		$request = $this->client->post("/login.php");
+		$request->setHeader('User-Agent', $this->useragent);
+
+		//Add our data transmission - MAL requires the XML content to be in a variable named "data"
+		$request->setPostField('username', $username);
+		$request->setPostField('password', $password);
+		$request->setPostField('sublogin', ' Login ');
+
+		// send request
+		$request->send();
 	}
 
 	public function fetch($url, $username = null, $password = null) {
