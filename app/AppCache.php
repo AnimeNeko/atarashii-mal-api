087fa9d87498cd6b72e393f41d093f8de69833e0
Michael Johnson <youngmug@animeneko.net>
2014-01-11 12:55:13 -0800

    Enable HTTP Cache
    Turn on the AppCache kernel in Symfony. This will allow the built-in
    reverse-proxy to cache data. Also, it’ll later let us use other cache
    solutions (Varnish, CloudFlare, etc.) should we wish.
    
    ref #22

--- app/AppCache.php
+++ app/AppCache.php
@@ -6,4 +6,15 @@ use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
 
 class AppCache extends HttpCache
 {
+	protected function getOptions()
+	{
+		return array(
+			'default_ttl'            => 0,
+			'private_headers'        => array('Authorization', 'Cookie'),
+			'allow_reload'           => true,
+			'allow_revalidate'       => true,
+			'stale_while_revalidate' => 2,
+			'stale_if_error'         => 60,
+		);
+	}
 }
