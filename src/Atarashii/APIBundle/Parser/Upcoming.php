bd586b9fa26478a047a4bfca3ca941b1b196b9dd
Michael Johnson <youngmug@animeneko.net>
2014-01-22 10:21:59 -0800

    Fix Error on Dates with ?
    Updated the logic check to bypass dates that are given on MAL that
    contain a question mark “?”. Removed the explicit set to null, since the
    values are null by default in the model. Also did a bit of style cleanup
    and a quick spelling fix.

--- src/Atarashii/APIBundle/Parser/Upcoming.php
+++ src/Atarashii/APIBundle/Parser/Upcoming.php
@@ -18,7 +18,7 @@ class Upcoming
         $mediaitems = $crawler->filter('#horiznav_nav')->nextAll()->filterXPath('./div/table/tr');
 
         foreach ($mediaitems as $item) {
-            //tricky methode to skip the menu bar which is also a <tr></tr>
+            //tricky method to skip the menu bar which is also a <tr></tr>
             if ($menubar == true) {
                 $menubar = false;
             } else {
@@ -45,10 +45,11 @@ class Upcoming
         }
 
         //Pull out all the common parts
-        $media->id = (int) str_replace('#sarea','',$crawler->filter('a')->attr('id'));
+        $media->id = (int) str_replace('#sarea', '', $crawler->filter('a')->attr('id'));
         $media->title = $crawler->filter('strong')->text();
+
         //I removed the 't' because it will else return a little image
-        $media->image_url = str_replace('t.j','.j',$crawler->filter('img')->attr('src'));
+        $media->image_url = str_replace('t.j', '.j', $crawler->filter('img')->attr('src'));
         $media->type = trim($crawler->filterXPath('//td[3]')->text());
 
         switch ($type) {
@@ -56,22 +57,18 @@ class Upcoming
                 //Custom parsing for anime
                 $media->episodes = (int) trim($crawler->filterXPath('//td[4]')->text());
 
-                //TODO add a way to format '?'
                 $start_date = trim($crawler->filterXPath('//td[6]')->text());
-                if (strpos($start_date,'?') == false && $start_date !== '-') {
+
+                if (strpos($start_date, '?') === false && $start_date !== '-') {
                     $start_date = DateTime::createFromFormat('m-d-y', $start_date)->format('Y-m-d');
-                } else {
-                    $start_date = null;
                 }
                 $media->start_date = $start_date;
 
-                //TODO add a way to format '?'
                 $end_date = trim($crawler->filterXPath('//td[7]')->text());
-                if (strpos($end_date,'?') == false && $end_date !== '-') {
+                if (strpos($end_date, '?') === false && $end_date !== '-') {
                     $end_date = DateTime::createFromFormat('m-d-y', $end_date)->format('Y-m-d');
-                } else {
-                    $end_date = null;
                 }
+
                 $media->end_date = $end_date;
                 $media->classification = trim($crawler->filterXPath('//td[9]')->text());
                 $media->members_score = (float) trim($crawler->filterXPath('//td[5]')->text());
@@ -84,8 +81,6 @@ class Upcoming
                 $media->volumes = (int) trim($crawler->filterXPath('//td[4]')->text());
                 $media->members_score = (float) trim($crawler->filterXPath('//td[6]')->text());
                 $media->synopsis = trim($crawler->filterXPath('//td[2]//div[2]')->text());
-                //$start_date = trim($crawler->filterXPath('//td[7]')->text());
-                //$end_date = trim($crawler->filterXPath('//td[8]')->text());
                 break;
             }
 
