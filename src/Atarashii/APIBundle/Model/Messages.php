0af4e520e62cbaa777e2a260e5e548f5bd658f7a
ratan12 <ratandhawtal@hotmail.com>
2014-05-13 14:26:45 +0200

    Convert Message Models to use Getters and Setters.

--- src/Atarashii/APIBundle/Model/Messages.php
+++ src/Atarashii/APIBundle/Model/Messages.php
@@ -5,20 +5,219 @@
 * @author    Ratan Dhawtal <ratandhawtal@hotmail.com>
 * @author    Michael Johnson <youngmug@animeneko.net>
 * @copyright 2014 Ratan Dhawtal and Michael Johnson
-* @license   http://www.apache.org/licenses/LICENSE-2.0 Apache Public License 2.0
+* @license   http://www.apache.org/licenses/LICENSE-2.0 Apache Private License 2.0
 */
 
 namespace Atarashii\APIBundle\Model;
 
 class Messages
 {
-    public $id; //The message ID used to send messages.
-    public $action_id; //The action ID (used to perform actions like delete).
-    public $thread_id; //The tread ID (used to view/send replies).
-    public $username; //username who send the message.
-    public $time; //The time when we received the message.
-    public $read; //The read status (true if you saw the message).
-    public $subject; //The subject of a message.
-    public $message; //The message.
-    public $preview; //The preview message.
+    private $id; //The message ID used to send messages.
+    private $action_id; //The action ID (used to perform actions like delete).
+    private $thread_id; //The tread ID (used to view/send replies).
+    private $username; //username who send the message.
+    private $time; //The time when we received the message.
+    private $read; //The read status (true if you saw the message).
+    private $subject; //The subject of a message.
+    private $message; //The message.
+    private $preview; //The preview message.
+
+
+    /**
+     * Set the id property
+     *
+     * @param int $id The id of messages.
+     *
+     * @return void
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * Get the id property.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+       return $this->id;
+    }
+
+    /**
+     * Set the action_id property
+     *
+     * @param int $action_id The action ID of messages.
+     *
+     * @return void
+     */
+    public function setActionId($action_id)
+    {
+        $this->action_id = $action_id;
+    }
+
+    /**
+     * Get the action_id property.
+     *
+     * @return int
+     */
+    public function getActionId()
+    {
+       return $this->action_id;
+    }
+
+    /**
+     * Set the thread_id property
+     *
+     * @param int $thread_id The thread ID of messages.
+     *
+     * @return void
+     */
+    public function setThreadId($thread_id)
+    {
+        $this->thread_id = $thread_id;
+    }
+
+    /**
+     * Get the thread_id property.
+     *
+     * @return int
+     */
+    public function getThreadId()
+    {
+       return $this->thread_id;
+    }
+
+    /**
+     * Set the username property
+     *
+     * @param string $username The username of messages.
+     *
+     * @return void
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * Get the username property.
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+       return $this->username;
+    }
+
+    /**
+     * Set the time property
+     *
+     * @param string $time The time of messages.
+     *
+     * @return void
+     */
+    public function setTime($time)
+    {
+        $this->time = $time;
+    }
+
+    /**
+     * Get the time property.
+     *
+     * @return string
+     */
+    public function getTime()
+    {
+       return $this->time;
+    }
+
+    /**
+     * Set the read property
+     *
+     * @param boolean $read The read status of messages.
+     *
+     * @return void
+     */
+    public function setRead($read)
+    {
+        $this->read = $read;
+    }
+
+    /**
+     * Get the read property.
+     *
+     * @return boolean
+     */
+    public function getRead()
+    {
+       return $this->read;
+    }
+
+    /**
+     * Set the subject property
+     *
+     * @param string $subject The subject of messages.
+     *
+     * @return void
+     */
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+    }
+
+    /**
+     * Get the subject property.
+     *
+     * @return string
+     */
+    public function getSubject()
+    {
+       return $this->subject;
+    }
+
+    /**
+     * Set the message property
+     *
+     * @param string $message The message of messages.
+     *
+     * @return void
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the message property.
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+       return $this->message;
+    }
+
+    /**
+     * Set the preview property
+     *
+     * @param string $preview The preview of messages.
+     *
+     * @return void
+     */
+    public function setPreview($preview)
+    {
+        $this->preview = $preview;
+    }
+
+    /**
+     * Get the preview property.
+     *
+     * @return string
+     */
+    public function getPreview()
+    {
+       return $this->preview;
+    }
 }
--- src/Atarashii/APIBundle/Parser/MessagesParser.php
+++ src/Atarashii/APIBundle/Parser/MessagesParser.php
@@ -43,40 +43,40 @@ class MessagesParser
         # Message id.
         # Example:
         # <input type="checkbox" id="msgchecker00000000" name="msg[]" value="0-0000000">
-        $message->id = (int) str_replace('?go=read&id=', '', $crawler->filter('div[class="mym_subject"] a')->attr('href'));
+        $message->setId((int) str_replace('?go=read&id=', '', $crawler->filter('div[class="mym_subject"] a')->attr('href')));
 
         # Action id.
         # Example:
         # <input type="checkbox" id="msgchecker00000000" name="msg[]" value="0-0000000">
-        $message->action_id = (int) str_replace('msgchecker', '', $crawler->filter('div[class="mym_checkboxes"] input')->attr('id'));
+        $message->setActionId((int) str_replace('msgchecker', '', $crawler->filter('div[class="mym_checkboxes"] input')->attr('id')));
 
         # Username of the sender.
         # Example:
         # <a href="http://myanimelist.net/profile/ratan12">ratan12</a>
-        $message->username = $crawler->filter('div[class="mym_user"] a')->text();
+        $message->setUsername($crawler->filter('div[class="mym_user"] a')->text());
 
         # Thread id.
         # Example:
         # <a href="?go=send&replyid=00000000&threadid=00000000&toname=xxxx">Reply</a>
-        $message->thread_id = (int) str_replace('&toname='.$message->username,'',str_replace('?go=send&replyid='.$message->action_id.'&threadid=', '', $crawler->filter('div[class="mym_actions"] a')->attr('href')));
+        $message->setThreadId((int) str_replace('&toname='.$message->getUsername(),'',str_replace('?go=send&replyid='.$message->GetActionId().'&threadid=', '', $crawler->filter('div[class="mym_actions"] a')->attr('href'))));
 
         # Time of the received message.
         # Example:
         # <small>50 minutes ago</small>
-        $message->time = $crawler->filter('div[class="mym_user"] small')->text();
+        $message->setTime($crawler->filter('div[class="mym_user"] small')->text());
 
         # Read (if the user has read this message).
-        $message->read = $read;
+        $message->setRead($read);
 
         # Subject.
         # Example:
         # <div style="margin-bottom: 4px; font-weight: bold;">Subject example</div>
-        $message->subject = $crawler->filter('div[class="mym_subject"] > a')->text();
+        $message->setSubject($crawler->filter('div[class="mym_subject"] > a')->text());
 
         # Preview message.
         # Example:
         # <a href="?go=read&id=5665695" class="lightLink">this is the first rule of an awesome message!</a>
-        $message->preview = $crawler->filter('div[class="mym_subject"] a[class="lightLink"]')->text();
+        $message->setPreview($crawler->filter('div[class="mym_subject"] a[class="lightLink"]')->text());
 
         return $message;
     }
@@ -88,38 +88,38 @@ class MessagesParser
 
         $message = new Messages();
 
-        $message->id = (int) $id;
+        $message->setId((int) $id);
 
         # Action id of the message.
         # Example:
         # <input type="button" onclick="document.location='/mymessages.php?go=send&replyid=21193061&threadid=16092543&toname=Ratan12'" class="inputButton" value=" Reply ">
         $del_button = "window.location='/mymessages.php?go=delete&id=";
         $del_button = str_replace($del_button, '', $crawler->filter('td div input')->eq(2)->attr('onclick'));
-        $message->action_id = (int) str_replace("'","",$del_button);
+        $message->setActionId((int) str_replace("'","",$del_button));
 
 
         # Thread id of the message.
         # Example:
         # <a href="?go=read&id=0000000&threadid=00000000">
-        $message->thread_id = (int) str_replace('?go=read&id='.$id.'&threadid=','',$crawler->filter('div div div div a')->attr('href'));
+        $message->setThreadId((int) str_replace('?go=read&id='.$id.'&threadid=','',$crawler->filter('div div div div a')->attr('href')));
 
         # Username of the sender.
         # Example:
         # <a href="http://myanimelist.net/profile/ratan12">ratan12</a>
-        $message->username = $crawler->filter('h2 a')->text();
+        $message->setUsername($crawler->filter('h2 a')->text());
 
         # Time of the received message.
         # Example:
         # <small>50 minutes ago</small>
-        $message->time = $crawler->filter('div small')->text();
+        $message->setTime($crawler->filter('div small')->text());
 
         # Subject.
         # Example:
         # <div style="margin-bottom: 4px; font-weight: bold;">re: coolmessage</div>
-        $message->subject = $crawler->filter('td div')->eq(1)->text();
+        $message->setSubject($crawler->filter('td div')->eq(1)->text());
 
         # Message.
-        $message->message = str_replace('Message Sent from '.$message->username.$message->time.$message->subject, '', $crawler->filter('tr td')->text());
+        $message->setMessage(str_replace('Message Sent from '.$message->getUsername().$message->getTime().$message->getSubject(), '', $crawler->filter('tr td')->text()));
 
         return $message;
     }
