91e51bfa2af9fdbeadb98aa3ba258d68c92a0172
ratan12 <ratandhawtal@hotmail.com>
2014-10-07 17:43:17 +0200

    Fix wrong object name: Adaptation & Related (Manga)
    Due a C&P mistake we forgot to change "manga_id" into "anime_id".

--- src/Atarashii/APIBundle/Parser/MangaParser.php
+++ src/Atarashii/APIBundle/Parser/MangaParser.php
@@ -242,7 +242,7 @@ class MangaParser
                     foreach ($relateditems as $item) {
                         if (preg_match('/<a href="(\/anime\/(\d+)\/.*?)">(.+?)<\/a>/', $item, $itemparts)) {
                             $itemarray = array();
-                            $itemarray['manga_id'] = $itemparts[2];
+                            $itemarray['anime_id'] = $itemparts[2];
                             $itemarray['title'] = $itemparts[3];
                             $itemarray['url'] = 'http://myanimelist.net'.$itemparts[1];
                             $mangarecord->setAnimeAdaptations($itemarray);
@@ -257,7 +257,7 @@ class MangaParser
                     foreach ($relateditems as $item) {
                         if (preg_match('/<a href="(\/manga\/(\d+)\/.*?)">(.+?)<\/a>/', $item, $itemparts)) {
                             $itemarray = array();
-                            $itemarray['manga_id'] = $itemparts[2];
+                            $itemarray['anime_id'] = $itemparts[2];
                             $itemarray['title'] = $itemparts[3];
                             $itemarray['url'] = 'http://myanimelist.net'.$itemparts[1];
                             $mangarecord->setRelatedManga($itemarray);
