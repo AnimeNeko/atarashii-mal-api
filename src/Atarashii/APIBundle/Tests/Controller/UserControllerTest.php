600579663c0f0b7d25baa949a132d44288b29d1c
Michael Johnson <motokochan@animeneko.net>
2015-10-16 16:42:52 -0700

    Fix Broken Live Profile Test
    MAL removed the double-spaces in user's birthdays and no longer shows
    list view counts. Adjust the test to correct the expectation.

--- src/Atarashii/APIBundle/Tests/Controller/UserControllerTest.php
+++ src/Atarashii/APIBundle/Tests/Controller/UserControllerTest.php
@@ -26,17 +26,10 @@ class UserControllerTest extends WebTestCase
 
         $this->assertNotNull($content->details->last_online); //This value changes based on the relative time MAL provides, so we can only check that something exists.
         $this->assertEquals('Female', $content->details->gender);
-        $this->assertEquals('December  1, 1983', $content->details->birthday); //Yes, there are two spaces for single-digit days.
+        $this->assertEquals('December 1, 1983', $content->details->birthday);
         $this->assertEquals('Hinata Inn', $content->details->location);
         $this->assertEquals('http://api.atarashiiapp.com', $content->details->website);
         $this->assertEquals('Member', $content->details->access_rank);
-
-        $this->assertInternalType('int', $content->details->anime_list_views);
-        $this->assertGreaterThanOrEqual(2, $content->details->anime_list_views);
-
-        $this->assertInternalType('int', $content->details->manga_list_views);
-        $this->assertGreaterThanOrEqual(2, $content->details->manga_list_views);
-
 
         $this->assertInternalType('float', $content->anime_stats->time_days);
         $this->assertGreaterThanOrEqual(0.1, $content->anime_stats->time_days);
