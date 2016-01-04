5fea697caa62f8f01fb4af91b27a364ef706418e
Michael Johnson <motokochan@animeneko.net>
2016-01-04 12:49:18 -0800

    Top and Upcoming Parser Tests
    Add tests for the upcoming and top parsers.

---
+++ src/Atarashii/APIBundle/Tests/Parser/TopTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Atarashii\APIBundle\Tests\Parser;
+
+use Atarashii\APIBundle\Parser\Top;
+
+class TopTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testParseAnime()
+    {
+        $animeContents = file_get_contents(__DIR__ . '/../InputSamples/anime-top.html');
+
+        $topList = Top::parse($animeContents, 'anime');
+
+        $this->assertInternalType('array', $topList);
+
+
+        $topItem = $topList[0];
+
+        $this->assertInstanceOf('Atarashii\APIBundle\Model\Anime', $topItem);
+
+        /* Although this test is against a static downloaded copy, the values are likely to change considerably
+         * each time the source file is updated. As such, mostly check the type and confirm a few things that
+         * shouldn't change.
+         */
+
+        //Do some basic type checking for things that should exist (not null)
+        $this->assertInternalType('int', $topItem->getId());
+        $this->assertInternalType('string', $topItem->getTitle());
+        $this->assertInternalType('string', $topItem->getImageUrl());
+        $this->assertInternalType('string', $topItem->getType());
+        $this->assertInternalType('float', $topItem->getMembersScore());
+        $this->assertInternalType('int', $topItem->getMembersCount());
+
+        //Check some data that should remain consistent
+        $this->assertStringStartsWith('http://cdn.myanimelist.net/images/anime/', $topItem->getImageUrl());
+        $this->assertInstanceOf('\DateTime', new \DateTime($topItem->getStartDate()));
+        $this->assertGreaterThan(8, $topItem->getMembersScore());
+        $this->assertGreaterThan(200000, $topItem->getMembersCount());
+    }
+
+    public function testParseManga()
+    {
+        $mangaContents = file_get_contents(__DIR__ . '/../InputSamples/manga-top.html');
+
+        $topList = Top::parse($mangaContents, 'manga');
+
+        $this->assertInternalType('array', $topList);
+
+
+        $topItem = $topList[0];
+
+        $this->assertInstanceOf('Atarashii\APIBundle\Model\Manga', $topItem);
+
+        /* Although this test is against a static downloaded copy, the values are likely to change considerably
+         * each time the source file is updated. As such, mostly check the type and confirm a few things that
+         * shouldn't change.
+         */
+
+        //Do some basic type checking for things that should exist (not null)
+        $this->assertInternalType('int', $topItem->getId());
+        $this->assertInternalType('string', $topItem->getTitle());
+        $this->assertInternalType('string', $topItem->getImageUrl());
+        $this->assertInternalType('float', $topItem->getMembersScore());
+        $this->assertInternalType('int', $topItem->getMembersCount());
+
+        //Check some data that should remain consistent
+        $this->assertStringStartsWith('http://cdn.myanimelist.net/images/manga/', $topItem->getImageUrl());
+        $this->assertGreaterThan(8, $topItem->getMembersScore());
+        $this->assertGreaterThan(50000, $topItem->getMembersCount());
+    }
+}
---
+++ src/Atarashii/APIBundle/Tests/Parser/UpcomingTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Atarashii\APIBundle\Tests\Parser;
+
+use Atarashii\APIBundle\Parser\Upcoming;
+
+class UpcomingTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testParseAnime()
+    {
+        $animeContents = file_get_contents(__DIR__ . '/../InputSamples/anime-upcoming.html');
+
+        $upcomingList = Upcoming::parse($animeContents, 'anime');
+
+        $this->assertInternalType('array', $upcomingList);
+
+
+        $upcomingItem = $upcomingList[0];
+
+        $this->assertInstanceOf('Atarashii\APIBundle\Model\Anime', $upcomingItem);
+
+        /* Although this test is against a static downloaded copy, the values are likely to change considerably
+         * each time the source file is updated. As such, mostly check the type and confirm a few things that
+         * shouldn't change.
+         */
+
+        //Do some basic type checking for things that should exist (not null)
+        $this->assertInternalType('int', $upcomingItem->getId());
+        $this->assertInternalType('string', $upcomingItem->getTitle());
+        $this->assertInternalType('string', $upcomingItem->getImageUrl());
+        $this->assertInternalType('string', $upcomingItem->getType());
+        $this->assertInternalType('string', $upcomingItem->getSynopsis());
+        $this->assertInternalType('string', $upcomingItem->getStartDate());
+
+        //Check some data that should remain consistent
+        $this->assertStringStartsWith('http://cdn.myanimelist.net/images/anime/', $upcomingItem->getImageUrl());
+        $this->assertInstanceOf('\DateTime', new \DateTime($upcomingItem->getStartDate()));
+
+
+        // Get some older titles to test date parsing and wrapping for two-digit years
+        $animeContents = file_get_contents(__DIR__ . '/../InputSamples/anime-upcoming-1930.html');
+
+        $upcomingList = Upcoming::parse($animeContents, 'anime');
+
+        $this->assertInternalType('array', $upcomingList);
+
+        $upcomingItem = $upcomingList[0];
+
+        $this->assertInstanceOf('Atarashii\APIBundle\Model\Anime', $upcomingItem);
+        $this->assertStringStartsWith('1930', $upcomingItem->getStartDate());
+    }
+
+    public function testParseManga()
+    {
+        $mangaContents = file_get_contents(__DIR__ . '/../InputSamples/manga-upcoming.html');
+
+        $upcomingList = Upcoming::parse($mangaContents, 'manga');
+
+        $this->assertInternalType('array', $upcomingList);
+
+
+        $upcomingItem = $upcomingList[0];
+
+        $this->assertInstanceOf('Atarashii\APIBundle\Model\Manga', $upcomingItem);
+
+        /* Although this test is against a static downloaded copy, the values are likely to change considerably
+         * each time the source file is updated. As such, mostly check the type and confirm a few things that
+         * shouldn't change.
+         */
+
+        //Do some basic type checking for things that should exist (not null)
+        $this->assertInternalType('int', $upcomingItem->getId());
+        $this->assertInternalType('string', $upcomingItem->getTitle());
+        $this->assertInternalType('string', $upcomingItem->getImageUrl());
+        $this->assertInternalType('string', $upcomingItem->getType());
+        $this->assertInternalType('string', $upcomingItem->getSynopsis());
+
+        //Check some data that should remain consistent
+        $this->assertStringStartsWith('http://cdn.myanimelist.net/images/manga/', $upcomingItem->getImageUrl());
+    }
+
+}
