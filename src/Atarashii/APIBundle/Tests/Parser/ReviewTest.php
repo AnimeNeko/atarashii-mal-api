9a09dbece6be3e5a4d95081e6dfa42b482af3c4b
Michael Johnson <motokochan@animeneko.net>
2016-01-05 19:25:36 -0800

    Add Reviews Unit Tests
    Implement unit tests for anime and manga reviews.

---
+++ src/Atarashii/APIBundle/Tests/Parser/ReviewTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Atarashii\APIBundle\Tests\Parser;
+
+use Atarashii\APIBundle\Parser\ReviewParser;
+
+class ReviewTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testParse()
+    {
+        $reviewContents = file_get_contents(__DIR__ . '/../InputSamples/anime-1887-reviews.html');
+
+        $reviews = ReviewParser::parse($reviewContents, 'A');
+
+        $this->assertInternalType('array', $reviews);
+
+        $review = $reviews[0];
+
+        $this->assertInstanceOf('Atarashii\APIBundle\Model\Review', $review);
+        $this->assertInstanceOf('\DateTime', new \DateTime($review->getDate()));
+
+        $this->assertInternalType('int', $review->getRating());
+        $this->assertGreaterThanOrEqual('1', $review->getRating());
+        $this->assertLessThanOrEqual('10', $review->getRating());
+
+        $this->assertInternalType('string', $review->getUsername());
+        $this->assertInternalType('int', $review->getHelpful());
+
+        $this->assertInternalType('int', $review->getEpisodes());
+        $this->assertNull($review->getChapters());
+
+
+        $reviewContents = file_get_contents(__DIR__ . '/../InputSamples/manga-11977-reviews.html');
+
+        $reviews = ReviewParser::parse($reviewContents, 'M');
+
+        $this->assertInternalType('array', $reviews);
+
+        $review = $reviews[0];
+
+        $this->assertInstanceOf('Atarashii\APIBundle\Model\Review', $review);
+        $this->assertInstanceOf('\DateTime', new \DateTime($review->getDate()));
+
+        $this->assertInternalType('int', $review->getRating());
+        $this->assertGreaterThanOrEqual('1', $review->getRating());
+        $this->assertLessThanOrEqual('10', $review->getRating());
+
+        $this->assertInternalType('string', $review->getUsername());
+        $this->assertInternalType('int', $review->getHelpful());
+
+        $this->assertInternalType('int', $review->getChapters());
+        $this->assertNull($review->getEpisodes());
+    }
+}
