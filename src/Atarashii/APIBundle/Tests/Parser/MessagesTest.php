9799c420b4a192cee6b0ccc9dd3cab284c7984c1
Michael Johnson <motokochan@animeneko.net>
2016-01-06 20:40:26 -0800

    Message Unit Tests
    Add unit tests for MAL messages.

---
+++ src/Atarashii/APIBundle/Tests/Parser/MessagesTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Atarashii\APIBundle\Tests\Messages;
+
+use Atarashii\APIBundle\Parser\Messages;
+use Atarashii\APIBundle\Parser\MessagesParser;
+
+class MessagesTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testParse()
+    {
+        $messageIndex = file_get_contents(__DIR__ . '/../InputSamples/user-messages-list.html');
+
+        $messages = MessagesParser::parse($messageIndex);
+
+        $this->assertInternalType('array', $messages);
+        $this->assertInternalType('int', $messages['pages']);
+        $this->assertInternalType('array', $messages['list']);
+
+        $message = $messages['list'][0];
+
+        $this->assertInstanceOf('Atarashii\APIBundle\Model\Messages', $message);
+
+        //Do some basic type checking for things that should exist (not null)
+        $this->assertInternalType('int', $message->getId());
+        $this->assertInternalType('int', $message->getActionId());
+        $this->assertInternalType('int', $message->getThreadId());
+        $this->assertInternalType('string', $message->getUsername());
+        $this->assertInternalType('bool', $message->getRead());
+        $this->assertInternalType('string', $message->getSubject());
+        $this->assertNull($message->getMessage());
+        $this->assertInternalType('string', $message->getPreview());
+
+        $messageContent = file_get_contents(__DIR__ . '/../InputSamples/user-message.html');
+
+        $message = MessagesParser::parseMessage($messageContent, 1234);
+
+        $this->assertInstanceOf('Atarashii\APIBundle\Model\Messages', $message);
+
+        //Do some basic type checking for things that should exist (not null)
+        $this->assertInternalType('int', $message->getId());
+        $this->assertInternalType('string', $message->getUsername());
+        $this->assertInternalType('string', $message->getSubject());
+        $this->assertInternalType('string', $message->getMessage());
+        $this->assertNull($message->getPreview());
+    }
+
+}
