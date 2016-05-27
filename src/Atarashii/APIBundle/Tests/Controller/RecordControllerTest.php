c3687ec0e7b69b27d428feba1dc6d98c98f86083
ratan12 <ratandhawtal@hotmail.com>
2016-05-27 14:46:51 +0200

    Add episodes and recommendations test

--- src/Atarashii/APIBundle/Tests/Controller/RecordControllerTest.php
+++ src/Atarashii/APIBundle/Tests/Controller/RecordControllerTest.php
@@ -36,10 +36,82 @@ class RecordControllerTest extends WebTestCase
         MangaRecordTest::testGetCastAction($this, $client);
     }
 
+    public function testGetRecsAction()
+    {
+        $client = $this->client;
+
+        // One Piece
+        $client->request('GET', '/2.1/anime/recs/21');
+        $this->checkRecs($client);
+        
+        // Death Note
+        $client->request('GET', '/2.1/manga/recs/21');
+        $this->checkRecs($client);
+    }
+
+    /**
+     * Test records for phpUnit.
+     * 
+     * This function was created to reduce the amount of codes.
+     * 
+     * @param $client The passed client
+     */
+    private function checkRecs($client) {
+        $rawContent = $client->getResponse()->getContent();
+        $statusCode = $client->getResponse()->getStatusCode();
+        $content = json_decode($rawContent);
+
+        $this->assertEquals(200, $statusCode);
+        $this->assertNotNull($content);
+
+        $this->assertInternalType('array', $content);
+        $item = $content[0];
+
+        $record = $item->item;
+
+        $this->assertInternalType('int', $record->id);
+        $this->assertGreaterThan(0, $record->id);
+        $this->assertInternalType('string', $record->title);
+        $this->assertContains('cdn.myanimelist.net', $record->image_url);
+
+        $recommendations = $item->recommendations;
+        $this->assertInternalType('array', $recommendations);
+        $recommendation = $recommendations[0];
+        $this->assertInternalType('string', $recommendation->information);
+        $this->assertInternalType('string', $recommendation->username);
+    }
+
+    public function testGetEpsAction()
+    {
+        $client = $this->client;
+
+        $client->request('GET', '/2.1/anime/episodes/21');
+
+        $rawContent = $client->getResponse()->getContent();
+        $statusCode = $client->getResponse()->getStatusCode();
+        $content = json_decode($rawContent);
+
+        $this->assertEquals(200, $statusCode);
+        $this->assertNotNull($content);
+
+        $this->assertInternalType('array', $content);
+        $anime = $content[0];
+
+        $this->assertInternalType('int', $anime->number);
+        $this->assertGreaterThan(0, $anime->number);
+        $this->assertInternalType('string', $anime->title);
+        $this->assertEquals('I\'m Luffy! The Man Who\'s Gonna Be King of the Pirates!', $anime->title);
+        $this->assertInstanceOf('\DateTime', new \DateTime($anime->air_date));
+
+        $this->assertEquals('Ore wa Luffy! Kaizoku Ou ni Naru Otoko Da!', $anime->other_titles->english[0]);
+
+        $this->assertEquals('俺はルフィ!海賊王になる男だ!', $anime->other_titles->japanese[0]);
+    }
+
     public function testGetScheduleAction()
     {
         $client = $this->client;
-        
+
         $client->request('GET', '/2.1/anime/schedule');
 
         $rawContent = $client->getResponse()->getContent();
@@ -50,9 +122,12 @@ class RecordControllerTest extends WebTestCase
         $this->assertNotNull($content);
 
         $item = $content->monday;
+        $this->assertInternalType('array', $item);
+        
         $item = $item[0];
 
         $this->assertInternalType('int', $item->id);
+        $this->assertGreaterThan(0, $item->id);
         $this->assertInternalType('string', $item->title);
         $this->assertContains('cdn.myanimelist.net', $item->image_url);
         $this->assertInternalType('int', $item->episodes);
