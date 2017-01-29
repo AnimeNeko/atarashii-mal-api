1ba829524f7432b25d39d88a1a2f4808c10c4223
Michael Johnson <motokochan@animeneko.net>
2017-01-29 04:16:44 -0800

    Refactor Sample Update Command
    Reduce repeated code in sample update command by moving the pages to a static array and looping with the download code.

--- src/Atarashii/APIBundle/Command/UpdateInputSamplesCommand.php
+++ src/Atarashii/APIBundle/Command/UpdateInputSamplesCommand.php
@@ -4,9 +4,10 @@
 *
 * @author    Ratan Dhawtal <ratandhawtal@hotmail.com>
 * @author    Michael Johnson <youngmug@animeneko.net>
-* @copyright 2014-2016 Ratan Dhawtal and Michael Johnson
+* @copyright 2014-2017 Ratan Dhawtal and Michael Johnson
 * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache Public License 2.0
 */
+
 namespace Atarashii\APIBundle\Command;
 
 use GuzzleHttp\Exception\ClientException;
@@ -19,6 +20,59 @@ use Symfony\Component\Filesystem\Filesystem;
 
 class UpdateInputSamplesCommand extends ContainerAwareCommand
 {
+    private static $anonPages = [
+        ['Anime 47 (Akira)', '/anime/47', '/anime-47.html'],
+        ['Anime 10236 (Kagee Grimm Douwa)', '/anime/10236', '/anime-10236.html'],
+        ['Anime 10758 (Momotarou)', '/anime/10758', '/anime-10758.html'],
+        ['Anime 18617 (Girls und Panzer Movie)', '/anime/18617', '/anime-18617.html'],
+        ['Manga 137 (R.O.D: Read or Die)', '/manga/137', '/manga-137.html'],
+        ['Manga 44347 (One-Punch Man)', '/manga/44347', '/manga-44347.html'],
+        ['Reviews for Anime 1887 (Lucky Star)', '/anime/1887/_/reviews', '/anime-1887-reviews.html'],
+        ['Reviews for Manga 11977 (Bambino!)', '/manga/11977/_/reviews', '/manga-11977-reviews.html'],
+        ['Recommendations for Anime 21 (One Piece)', '/anime/21/_/userrecs', '/anime-21-recs.html'],
+        ['Recommendations for Manga 21 (Death Note)', '/manga/21/_/userrecs', '/manga-21-recs.html'],
+        ['Characters for Anime 1887 (Lucky Star)', '/anime/1887/_/characters', '/anime-1887-cast.html'],
+        ['Episodes for Anime 21 (One Piece)', '/anime/21/_/episode', '/anime-21-eps.html'],
+        ['Top Anime List', '/topanime.php?limit=0', '/anime-top.html'],
+        ['Top Manga List', '/topmanga.php?limit=0', '/manga-top.html'],
+        ['Upcoming Anime List for January 1930', '/anime.php?sd=01&sm=01&sy=1930&em=0&ed=0&ey=0&o=2&w=&c[]=a&c[]=d&c[]=a&c[]=b&c[]=c&c[]=d&c[]=e&c[]=f&c[]=g&cv=1&show=0', '/anime-upcoming-1930.html'],
+        ['Upcoming Anime List for June 2016', '/anime.php?sd=09&sm=06&sy=2016&em=0&ed=0&ey=0&o=2&w=&c[]=a&c[]=d&c[]=a&c[]=b&c[]=c&c[]=d&c[]=e&c[]=f&c[]=g&cv=1&show=0', '/anime-upcoming.html'],
+        ['Upcoming Manga List for June 2016', '/manga.php?sd=09&sm=06&sy=2016&em=0&ed=0&ey=0&o=2&w=&c[]=a&c[]=d&c[]=a&c[]=b&c[]=c&c[]=d&c[]=e&c[]=f&c[]=g&cv=1&show=0', '/manga-upcoming.html'],
+        ['Anime Season Schedule', '/anime/season/schedule', '/schedule-26052016.html'],
+        ['Forum Index', '/forum/', '/forum-index.html'],
+        ['Forum Board 14 (MAL Guidelines & FAQ)', '/forum/?board=14', '/forum-board-14.html'],
+        ['Forum Sub-Board 2 (Anime DB)', '/forum/?subboard=2', '/forum-sub-2.html'],
+        ['Forum Topic 516059 (Site & Forum Guidelines)', '/forum/?topicid=516059', '/forum-topic-516059.html'],
+        ['Person 10 (Bosch, Johnny Yong)', '/people/10', '/people-10.html'],
+        ['Person 159 (Rial, Monica)', '/people/159', '/people-159.html'],
+        ['Person 185 (Hanazawa, Kana)', '/people/185', '/people-185.html'],
+        ['Person 1870 (Miyazaki, Hayao)', '/people/1870', '/people-1870.html'],
+        ['Person 2608 (Miyamoto, Kano)', '/people/2608', '/people-2608.html'],
+        ['Person 7277 (Ikimono-gakari)', '/people/7277', '/people-7277.html'],
+        ['Person 10145 (huke)', '/people/10145', '/people-10145.html'],
+        ['Person 11746 (ClariS)', '/people/11746', '/people-11746.html'],
+        ['Profile for User Xinil', '/profile/xinil', '/profile-xinil.html'],
+        ['Profile for User Motoko', '/profile/motoko', '/profile-motokochan.html'],
+        ['Friendlist for User Motoko', '/profile/motoko/friends', '/profile-motokochan-friends.html'],
+        ['History for User Motoko', '/history/motoko', '/history-motokochan.html'],
+    ];
+
+    //NOTE: PHP doesn't allow expressions in default values, so we use placeholders.
+    //Use __MESSAGEID__ for the user message ID in the URL.
+    //We will replace these in the function.
+    private static $authPages = [
+        ['Anime 1887 Personal (Lucky Star)', '/anime/1887', '/anime-1887-mine.html'],
+        ['Manga 11977 Personal (Bambino!)', '/manga/11977', '/manga-11977-mine.html'],
+        ['Anime 1689 Personal Details (5cm/s)', '/ownlist/anime/1689/edit?hideLayout', '/anime-1689-mine-detailed.html'],
+        ['Manga 11977 Personal Details (Bambino!)', '/ownlist/manga/11977/edit?hideLayout', '/manga-11977-mine-detailed.html'],
+        ['Manga 17074 Personal Details (We Can Fly!)', '/ownlist/manga/17074/edit?hideLayout', '/manga-17074-mine-detailed.html'],
+        ['Anime 1689 History (5cm/s)', '/ajaxtb.php?detailedaid=1689', '/anime-1689-history.html'],
+        ['Manga 11977 History (Bambino!)', '/ajaxtb.php?detailedmid=11977', '/manga-11977-history.html'],
+        ['Account Profile Details', '/editprofile.php', '/editprofile.html'],
+        ['User Messages List', '/mymessages.php', '/user-messages-list.html'],
+        ['User Message', '/mymessages.php?go=read&id=__MESSAGEID__', '/user-message.html'],
+    ];
+
     protected function configure()
     {
         $this
@@ -38,7 +92,7 @@ class UpdateInputSamplesCommand extends ContainerAwareCommand
 
         $downloader = $this->getContainer()->get('atarashii_api.communicator');
         $rootDir = $this->getContainer()->getParameter('kernel.root_dir');
-        $outputDir = $rootDir . '/../src/Atarashii/APIBundle/Tests/InputSamples';
+        $outputDir = $rootDir.'/../src/Atarashii/APIBundle/Tests/InputSamples';
 
         $io->title('Input Sample Updater');
 
@@ -50,7 +104,7 @@ class UpdateInputSamplesCommand extends ContainerAwareCommand
         } catch (ClientException $e) {
             $response = $e->getResponse();
 
-            if ($response->getStatusCode() == 404) {
+            if ($response->getStatusCode() === 404) {
                 $io->error('Page doesn\'t exist anymore (404).');
             } else {
                 throw $e;
@@ -60,180 +114,22 @@ class UpdateInputSamplesCommand extends ContainerAwareCommand
         }
     }
 
-    private function fetchAnonPages($io, $fs, $downloader, $outputDir) {
+    private function fetchAnonPages($io, $fs, $downloader, $outputDir)
+    {
         $io->section('Fetching pages that don\'t need auth.');
 
-        $io->progressStart(34);
+        $io->progressStart(count(self::$anonPages));
 
-        $io->progressAdvance();
-        $io->text('Anime 47 (Akira)');
-        $pageContent = $downloader->fetch('/anime/47');
-        $fs->dumpFile($outputDir . '/anime-47.html', $pageContent);
+        foreach (self::$anonPages as $page) {
+            $pageTitle = $page[0];
+            $pageUrl = $page[1];
+            $pageFilename = $page[2];
 
-        $io->progressAdvance();
-        $io->text('Anime 10236 (Kagee Grimm Douwa)');
-        $pageContent = $downloader->fetch('/anime/10236');
-        $fs->dumpFile($outputDir . '/anime-10236.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Anime 10758 (Momotarou)');
-        $pageContent = $downloader->fetch('/anime/10758');
-        $fs->dumpFile($outputDir . '/anime-10758.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Anime 18617 (Girls und Panzer Movie)');
-        $pageContent = $downloader->fetch('/anime/18617');
-        $fs->dumpFile($outputDir . '/anime-18617.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Manga 137 (R.O.D: Read or Die)');
-        $pageContent = $downloader->fetch('/manga/137');
-        $fs->dumpFile($outputDir . '/manga-137.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Manga 44347 (One-Punch Man)');
-        $pageContent = $downloader->fetch('/manga/44347');
-        $fs->dumpFile($outputDir . '/manga-44347.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Reviews for Anime 1887 (Lucky Star)');
-        $pageContent = $downloader->fetch('/anime/1887/_/reviews');
-        $fs->dumpFile($outputDir . '/anime-1887-reviews.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Reviews for Manga 11977 (Bambino!)');
-        $pageContent = $downloader->fetch('/manga/11977/_/reviews');
-        $fs->dumpFile($outputDir . '/manga-11977-reviews.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Recommendations for Anime 21 (One Piece)');
-        $pageContent = $downloader->fetch('/anime/21/_/userrecs');
-        $fs->dumpFile($outputDir . '/anime-21-recs.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Recommendations for Manga 21 (Death Note)');
-        $pageContent = $downloader->fetch('/manga/21/_/userrecs');
-        $fs->dumpFile($outputDir . '/manga-21-recs.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Characters for Anime 1887 (Lucky Star)');
-        $pageContent = $downloader->fetch('/anime/1887/_/characters');
-        $fs->dumpFile($outputDir . '/anime-1887-cast.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Episodes for Anime 21 (One Piece)');
-        $pageContent = $downloader->fetch('/anime/21/_/episode');
-        $fs->dumpFile($outputDir . '/anime-21-eps.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Top Anime List');
-        $pageContent = $downloader->fetch('/topanime.php?limit=0');
-        $fs->dumpFile($outputDir . '/anime-top.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Top Manga List');
-        $pageContent = $downloader->fetch('/topmanga.php?limit=0');
-        $fs->dumpFile($outputDir . '/manga-top.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Upcoming Anime List for January 1930');
-        $pageContent = $downloader->fetch('/anime.php?sd=01&sm=01&sy=1930&em=0&ed=0&ey=0&o=2&w=&c[]=a&c[]=d&c[]=a&c[]=b&c[]=c&c[]=d&c[]=e&c[]=f&c[]=g&cv=1&show=0');
-        $fs->dumpFile($outputDir . '/anime-upcoming-1930.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Upcoming Anime List for June 2016');
-        $pageContent = $downloader->fetch('/anime.php?sd=09&sm=06&sy=2016&em=0&ed=0&ey=0&o=2&w=&c[]=a&c[]=d&c[]=a&c[]=b&c[]=c&c[]=d&c[]=e&c[]=f&c[]=g&cv=1&show=0');
-        $fs->dumpFile($outputDir . '/anime-upcoming.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Upcoming Manga List for June 2016');
-        $pageContent = $downloader->fetch('/manga.php?sd=09&sm=06&sy=2016&em=0&ed=0&ey=0&o=2&w=&c[]=a&c[]=d&c[]=a&c[]=b&c[]=c&c[]=d&c[]=e&c[]=f&c[]=g&cv=1&show=0');
-        $fs->dumpFile($outputDir . '/manga-upcoming.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Anime Season Schedule');
-        $pageContent = $downloader->fetch('/anime/season/schedule');
-        $fs->dumpFile($outputDir . '/schedule-26052016.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Forum Index');
-        $pageContent = $downloader->fetch('/forum/');
-        $fs->dumpFile($outputDir . '/forum-index.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Forum Board 14 (MAL Guidelines & FAQ)');
-        $pageContent = $downloader->fetch('/forum/?board=14');
-        $fs->dumpFile($outputDir . '/forum-board-14.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Forum Sub-Board 2 (Anime DB)');
-        $pageContent = $downloader->fetch('/forum/?subboard=2');
-        $fs->dumpFile($outputDir . '/forum-sub-2.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Forum Topic 516059 (Site & Forum Guidelines)');
-        $pageContent = $downloader->fetch('/forum/?topicid=516059');
-        $fs->dumpFile($outputDir . '/forum-topic-516059.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Person 10 (Bosch, Johnny Yong)');
-        $pageContent = $downloader->fetch('/people/10');
-        $fs->dumpFile($outputDir . '/people-10.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Person 159 (Rial, Monica)');
-        $pageContent = $downloader->fetch('/people/159');
-        $fs->dumpFile($outputDir . '/people-159.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Person 185 (Hanazawa, Kana)');
-        $pageContent = $downloader->fetch('/people/185');
-        $fs->dumpFile($outputDir . '/people-185.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Person 1870 (Miyazaki, Hayao)');
-        $pageContent = $downloader->fetch('/people/1870');
-        $fs->dumpFile($outputDir . '/people-1870.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Person 2608 (Miyamoto, Kano)');
-        $pageContent = $downloader->fetch('/people/2608');
-        $fs->dumpFile($outputDir . '/people-2608.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Person 7277 (Ikimono-gakari)');
-        $pageContent = $downloader->fetch('/people/7277');
-        $fs->dumpFile($outputDir . '/people-7277.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Person 10145 (huke)');
-        $pageContent = $downloader->fetch('/people/10145');
-        $fs->dumpFile($outputDir . '/people-10145.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Person 11746 (ClariS)');
-        $pageContent = $downloader->fetch('/people/11746');
-        $fs->dumpFile($outputDir . '/people-11746.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Profile for User Xinil');
-        $pageContent = $downloader->fetch('/profile/xinil');
-        $fs->dumpFile($outputDir . '/profile-xinil.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Profile for User Motoko');
-        $pageContent = $downloader->fetch('/profile/motoko');
-        $fs->dumpFile($outputDir . '/profile-motokochan.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('Friendlist for User Motoko');
-        $pageContent = $downloader->fetch('/profile/motoko/friends');
-        $fs->dumpFile($outputDir . '/profile-motokochan-friends.html', $pageContent);
-
-        $io->progressAdvance();
-        $io->text('History for User Motoko');
-        $pageContent = $downloader->fetch('/history/motoko');
-        $fs->dumpFile($outputDir . '/history-motokochan.html', $pageContent);
+            $io->progressAdvance();
+            $io->text($pageTitle);
+            $pageContent = $downloader->fetch($pageUrl);
+            $fs->dumpFile($outputDir.$pageFilename, $pageContent);
+        }
     }
 
     private function fetchAuthPages($io, $fs, $downloader, $outputDir)
@@ -248,57 +144,18 @@ class UpdateInputSamplesCommand extends ContainerAwareCommand
 
         if ($username !== 'CHANGEME') {
             if ($downloader->cookieLogin($username, $password)) {
-                $io->progressStart(10);
+                $io->progressStart(count(self::$authPages));
 
-                $io->progressAdvance();
-                $io->text('Anime 1887 Personal (Lucky Star)');
-                $pageContent = $downloader->fetch('/anime/1887');
-                $fs->dumpFile($outputDir . '/anime-1887-mine.html', $pageContent);
+                foreach (self::$authPages as $page) {
+                    $pageTitle = $page[0];
+                    $pageUrl = str_replace('__MESSAGEID__', $messageId, $page[1]);
+                    $pageFilename = $page[2];
 
-                $io->progressAdvance();
-                $io->text('Manga 11977 Personal (Bambino!)');
-                $pageContent = $downloader->fetch('/manga/11977');
-                $fs->dumpFile($outputDir . '/manga-11977-mine.html', $pageContent);
-
-                $io->progressAdvance();
-                $io->text('Anime 1689 Personal Details (5cm/s)');
-                $pageContent = $downloader->fetch('/ownlist/anime/1689/edit?hideLayout');
-                $fs->dumpFile($outputDir . '/anime-1689-mine-detailed.html', $pageContent);
-
-                $io->progressAdvance();
-                $io->text('Manga 11977 Personal Details (Bambino!)');
-                $pageContent = $downloader->fetch('/ownlist/manga/11977/edit?hideLayout');
-                $fs->dumpFile($outputDir . '/manga-11977-mine-detailed.html', $pageContent);
-
-                $io->progressAdvance();
-                $io->text('Manga 17074 Personal Details (We Can Fly!)');
-                $pageContent = $downloader->fetch('/ownlist/manga/17074/edit?hideLayout');
-                $fs->dumpFile($outputDir . '/manga-17074-mine-detailed.html', $pageContent);
-
-                $io->progressAdvance();
-                $io->text('Anime 1689 History (5cm/s)');
-                $pageContent = $downloader->fetch('/ajaxtb.php?detailedaid=1689');
-                $fs->dumpFile($outputDir . '/anime-1689-history.html', $pageContent);
-
-                $io->progressAdvance();
-                $io->text('Manga 11977 History (Bambino!)');
-                $pageContent = $downloader->fetch('/ajaxtb.php?detailedmid=11977');
-                $fs->dumpFile($outputDir . '/manga-11977-history.html', $pageContent);
-
-                $io->progressAdvance();
-                $io->text('Account Profile Details (' . $username . ')');
-                $pageContent = $downloader->fetch('/editprofile.php');
-                $fs->dumpFile($outputDir . '/editprofile.html', $pageContent);
-
-                $io->progressAdvance();
-                $io->text('User Messages List');
-                $pageContent = $downloader->fetch('/mymessages.php');
-                $fs->dumpFile($outputDir . '/user-messages-list.html', $pageContent);
-
-                $io->progressAdvance();
-                $io->text('Message #' . $messageId);
-                $pageContent = $downloader->fetch('/mymessages.php?go=read&id=' . $messageId);
-                $fs->dumpFile($outputDir . '/user-message.html', $pageContent);
+                    $io->progressAdvance();
+                    $io->text($pageTitle);
+                    $pageContent = $downloader->fetch($pageUrl);
+                    $fs->dumpFile($outputDir.$pageFilename, $pageContent);
+                }
             } else {
                 $io->error('Username and password did not work. Please check that they are set correctly in the unit_testing parameters.');
             }
@@ -306,5 +163,4 @@ class UpdateInputSamplesCommand extends ContainerAwareCommand
             $io->note('Username and Password for user are not set. Cannot download updates for pages requiring auth.');
         }
     }
-
-    }
+}
